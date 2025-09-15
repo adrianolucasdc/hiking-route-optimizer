9b1b2bcf9bb739e1ab51c8303f8d9faffc6f1461
adrianolucasdc <adrianocaixeta5@gmail.com>
2025-09-15 20:52:56 -0300

    fix: docker configs

---
+++ backend/src/main/java/com/example/hiking_route/HikingRouteApplication.java
@@ -0,0 +1,13 @@
+package com.example.hiking_route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikingRouteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/hiking_route/HikingRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hiking_route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
