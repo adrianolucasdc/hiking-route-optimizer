bc8b5b6b00b85aab069b5447d4785c043be7d96a
Davi <daviramossousa@gmail.com>
2025-09-18 09:36:05 -0300

    feat: adicionado tailwind no projeto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <nav className="flex gap-4 p-4 bg-gray-100 shadow">
-        <Link to="/" className="text-blue-500 hover:underline">Home</Link>
-        <Link to="/about" className="text-blue-500 hover:underline">About</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-  );
+    <div className='text-red-500 text-[50px]'>
+      Hello World
+    </div>
+  )
 }
+
+export default App
--- frontend/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold text-blue-600">🏠 Página Inicial Oficial</h1>
-    </div>
-  )
-}
